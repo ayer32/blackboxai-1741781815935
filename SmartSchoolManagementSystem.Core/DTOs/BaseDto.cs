d5a136e2fe376cb5702dd5afd6244dd3070506c4
ayer32 <48725957+ayer32@users.noreply.github.com>
2025-03-12 18:03:21 +0545

    Add SmartSchoolManagementSystem.Core/DTOs/BaseDto.cs 1741781900354

---
+++ SmartSchoolManagementSystem.Core/DTOs/BaseDto.cs
@@ -0,0 +1,8 @@
+namespace SmartSchoolManagementSystem.Core.DTOs;
+
+public abstract class BaseDto
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
