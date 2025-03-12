823ff7c80f664dff91679228b1f5e66d36f0429e
ayer32 <48725957+ayer32@users.noreply.github.com>
2025-03-12 18:03:52 +0545

    Add SmartSchoolManagementSystem.Core/Interfaces/IRepository.cs 1741781931295

---
+++ SmartSchoolManagementSystem.Core/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+using System.Linq.Expressions;
+using SmartSchoolManagementSystem.Core.Entities;
+
+namespace SmartSchoolManagementSystem.Core.Interfaces;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    Task<T?> GetByIdAsync(Guid id);
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<IReadOnlyList<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+    Task<bool> ExistsAsync(Guid id);
+    Task<int> SaveChangesAsync();
+}
