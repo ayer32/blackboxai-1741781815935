4241c8528c5153e9912568ae8ed9f9c7b63eb262
ayer32 <48725957+ayer32@users.noreply.github.com>
2025-03-12 18:04:08 +0545

    Add SmartSchoolManagementSystem.Core/Entities/BaseEntity.cs 1741781948078

---
+++ SmartSchoolManagementSystem.Core/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+namespace SmartSchoolManagementSystem.Core.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; }
+}
